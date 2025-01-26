dc1545b0734918cf99e43f44ca0f492981ceb3e6
ammar-muslim <newamaremara@gmail.com>
2025-01-26 17:43:53 +0200

    initial commit for backend

--- src/api/subcategory/services/subcategory.js
+++ src/api/subcategory/services/subcategory.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service.
+ * subcategory service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::subcategory.subcategory');
